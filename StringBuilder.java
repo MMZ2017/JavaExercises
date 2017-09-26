57a24e6417c2e9cca9246118d4f347671ae05322
MZ <31194605+MMZ2017@users.noreply.github.com>
2017-09-25 21:34:38 -0400

    Create StringBuilder.java

---
+++ StringBuilder.java
@@ -0,0 +1,9 @@
+public class StringBuilder {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder("");
+		sb.append("Hellow.");
+		sb.append(" ");
+		sb.append("I am a skydiver.");
+		System.out.println(sb.toString());
+	}
+}
