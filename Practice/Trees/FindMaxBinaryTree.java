311d88d6b63066e2666418c58b72fd5d279b03fa
MZ <31194605+MMZ2017@users.noreply.github.com>
2017-11-10 16:49:48 -0500

    Create FindMaxBinaryTree.java

---
+++ Practice/Trees/FindMaxBinaryTree.java
@@ -0,0 +1,39 @@
+
+public class FindMaxBinaryTree {
+  
+  public static class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+      val = x;
+    }
+  }
+    
+  public static int findMax(TreeNode root) {
+    TreeNode current = root;
+    if (current == null) {
+      return Integer.MIN_VALUE;
+    } else {
+      return Math.max(Math.max(findMax(current.left), findMax(current.right)), current.val);
+    }
+  }
+ 
+  public static void main(String[] args) {
+    TreeNode node1 = new TreeNode(1);
+    TreeNode node2 = new TreeNode(2);
+    TreeNode node3 = new TreeNode(3);
+    TreeNode node4 = new TreeNode(4);
+    TreeNode node5 = new TreeNode(5);
+    TreeNode node6 = new TreeNode(6);
+    TreeNode node7 = new TreeNode(7);
+    node1.left = node2;
+    node1.right = node3;
+    node2.left = node4;
+    node2.right = node5;
+    node3.left = node6;
+    node5.left = node7;
+    
+    System.out.println(findMax(node1));  
+  }
+}
